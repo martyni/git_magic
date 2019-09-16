c0829bd3948cea1ed54571fca4d5653fecb819d4
Martyn Pratt <martyni.pratt@digital.homeoffice.gov.uk>
2019-09-16 14:38:28 +0100

    updated README and copy script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # git_magic
 cheeky git scripts for more automated workflow
+```
+cd git clone https://github.com/martyni/git_magic.git
+cd git_magic
+./copy.sh
+```
--- copy.sh
+++ copy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "Path to you project"
+echo 'Path to you project'
 read path 
 echo "copying config into your project $path/.git/" 
 cp -r git/* $path/.git/ 
+echo 
+echo 'You will need to re-add your git origin to your repo'
+echo "git remote add origin \"<remote_url>\""
